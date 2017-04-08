48c9f10aca85da65f305bbe4ca0d7402981551dd
Pepi Nurul Ichsani <pepinurul29@gmail.com>
2017-04-08 11:59:01 +0700

    Update Faktorial.java

--- Faktorial.java
+++ Faktorial.java
@@ -21,7 +21,7 @@ public class Faktorial {
 		//bagian atas
 		for(int x=n; x>=1; x--){
 			for(int y=x-1; y>=1; y--){
-				System.out.print(" ");
+				System.out.print("0");
 			}
 			for(int z=1; z<=((n+1)-x)*2; z++){
 				System.out.print("*");
@@ -32,7 +32,7 @@ public class Faktorial {
 		//bagian bawah
 		for(int i=n; i>=1; i--){
 			for(int k=1; k<=n-i; k++){
-				System.out.print(" ");
+				System.out.print("0");
 			}
 			for(int j=i*2; j>=1; j--){
 				System.out.print("*");
